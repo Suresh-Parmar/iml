0665321696d93b0d10a3f8c48e22a9a9d8feda25
djangir <djangir8875@gmail.com>
2023-09-22 22:49:47 +0530

    remove dropdown inactive value

---
+++ helpers/dropDownData.ts
@@ -0,0 +1,53 @@
+export const filterDataMulti = (
+  data: any,
+  label: any = "name",
+  value: any = "value",
+  plusLabel: any = "",
+  plusLabelStr: any = "",
+  sort: any = true
+) => {
+  let newData: any = [];
+  if (Array.isArray(data)) {
+    data.forEach((item: any) => {
+      if (!!item[label] && item.status) {
+        let obj = {
+          label: item[label],
+          value: item[value],
+        };
+
+        if (plusLabel) {
+          obj.label += " (" + plusLabel + " " + item[plusLabelStr] + ")";
+        }
+        newData.push(obj);
+      }
+    });
+  }
+
+  if (sort) {
+    let sortData = newData.sort((a: any, b: any) => {
+      let fa = a.label.toLowerCase(),
+        fb = b.label.toLowerCase();
+
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+    return sortData;
+  }
+
+  return newData;
+};
+
+export let filterDataSingle = (data: any, key: string): any[] => {
+  let newData: any[] = [];
+  data.forEach((item: any) => {
+    if (item[key] && item.status) {
+      newData.push(item[key]);
+    }
+  });
+  return newData.sort();
+};
