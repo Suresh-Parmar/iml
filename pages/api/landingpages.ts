fb5f80d1ee42e57dec1ff52da353aed2576b9e9e
djangir <djangir8875@gmail.com>
2023-11-26 19:29:19 +0530

    add sqlite db

--- pages/api/landingpages.ts
+++ pages/api/landingpages.ts
@@ -1,15 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
-import path from "path";
+import { NextApiResponse } from "next";
 import fetchData from "./fetchData";
-
-const apiFolderPath = path.resolve("./pages/api");
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
 
 export default async function handler(req: any, res: NextApiResponse) {
-  let datareq = req.body;
+  const datareq = req.body;
 
-  const jsonFileName = datareq?.collection_name + ".json" || "data.json";
-  const jsonFilePath = path.join(apiFolderPath, jsonFileName);
+  const db = await open({
+    filename: "localData",
+    driver: sqlite3.Database,
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS cached_data (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      collection_name TEXT,
+      data TEXT,
+      last_modified TEXT
+    );
+  `);
 
   let time: any = process.env.NEXT_PUBLIC_API_CACHE_TIMEOUT || 10;
   time = +time;
@@ -18,47 +27,51 @@ export default async function handler(req: any, res: NextApiResponse) {
   }
 
   if (req.method.toLowerCase() === "post") {
-    let finalData = "";
-    await fs.stat(jsonFilePath, async (err, stats) => {
-      if (err) {
-        let data: any = await fetchData(datareq);
+    try {
+      const cachedData = await db.get("SELECT * FROM cached_data WHERE collection_name = ?", datareq.collection_name);
 
-        data = await data?.data?.response;
+      const currentTime: any = new Date();
+      const savedTime: any = cachedData ? new Date(cachedData.last_modified) : null;
 
-        if (data) {
-          await fs.writeFile(jsonFilePath, JSON.stringify(data), (err) => {
-            if (err) throw err;
-            return;
-          });
+      let timeDifference: any = savedTime
+        ? Math.abs(currentTime.getTime() - savedTime.getTime())
+        : Number.MAX_SAFE_INTEGER;
+
+      timeDifference = Math.floor(timeDifference / 1000 / 60);
+
+      console.log(timeDifference, "timeDifference");
+
+      if (!cachedData || timeDifference > time) {
+        const data = await fetchData(datareq);
+        const responseData = data?.data?.response;
+
+        if (responseData) {
+          const now = new Date();
+
+          if (cachedData) {
+            await db.run(
+              "UPDATE cached_data SET data = ?, last_modified = ? WHERE collection_name = ?",
+              JSON.stringify(responseData),
+              now.toISOString(),
+              datareq.collection_name
+            );
+          } else {
+            await db.run(
+              "INSERT INTO cached_data (collection_name, data, last_modified) VALUES (?, ?, ?)",
+              datareq.collection_name,
+              JSON.stringify(responseData),
+              now.toISOString()
+            );
+          }
+
+          res.status(200).json(responseData);
         }
-        res.status(200).json(data);
-        return;
+      } else {
+        res.status(200).json(JSON.parse(cachedData?.data));
       }
-      const lastModified = stats.mtime;
-      const currentTime = new Date();
-      const timeDifference = currentTime.getTime() - lastModified.getTime();
-
-      if (timeDifference > time * 60 * 1000) {
-        let data: any = await fetchData(datareq);
-        data = await data?.data?.response;
-
-        if (data) {
-          await fs.writeFile(jsonFilePath, JSON.stringify(data), (err) => {
-            if (err) {
-              console.log(err);
-            }
-          });
-        }
-      }
-    });
-
-    await fs.readFile(jsonFilePath, "utf8", (err, data) => {
-      if (err) {
-        console.log("Error reading file from disk: ", err);
-        return;
-      }
-
-      res.status(200).json(JSON.parse(data));
-    });
+    } catch (err) {
+      console.error("Error querying or updating cache:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 }
