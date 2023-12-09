b7dc6911a01d0cf89772568799ef1139e52197e0
djangir <djangir8875@gmail.com>
2023-12-09 09:26:20 +0530

    fix pdf

--- components/profile/YourProduct.tsx
+++ components/profile/YourProduct.tsx
@@ -4,49 +4,18 @@ import { useSelector } from "react-redux";
 function YourProduct() {
   let userData = useSelector((state: any) => state?.data?.userData?.user);
 
-  // console.log(userData);
-
-  console.log(userData.class_id);
-
-  const yourProductJson = [
-    {
-      label: "Your Product",
-      placeholder: "Your Product",
-      type: "yourProductJson",
-      url: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
-    },
-    {
-      label: "Your Product",
-      placeholder: "Your Product",
-      url: "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-      type: "yourProductJson",
-    },
-    {
-      label: "Your Product",
-      placeholder: "Your Product",
-      url: "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-      type: "yourProductJson",
-    },
-    {
-      label: "Your Product",
-      placeholder: "Your Product",
-      url: "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-      type: "yourProductJson",
-    },
-  ];
+  const yourProductJson = userData?.myproducts || [];
   return (
-    <div>
-      <div style={{ marginBottom: "20px", fontSize: "30px" }}>Your Product</div>;
-      {yourProductJson.map((item, index) => (
-        <img
-          key={index}
-          className="ms-3 alt={item.label}  justify-align-item-center"
-          src={item.url}
-          width="270"
-          height="270"
-        />
-      ))}
-    </div>
+    <>
+      <div style={{ marginBottom: "20px", fontSize: "30px" }}>Your Product</div>
+      <div className="wrapperbox">
+        {yourProductJson.map((item: any, index: any) => (
+          <div key={index}>
+            <embed src={item.resourcefileurl + "#toolbar=0"} style={{ width: "70lvw", height: "70lvh" }}></embed>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
