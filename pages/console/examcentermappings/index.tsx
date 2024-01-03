acaad3c7d20a0cc526431944b64c36d6435dc9c2
djangir <djangir8875@gmail.com>
2024-01-03 22:25:09 +0530

    Add Pagination

--- pages/console/examcentermappings/index.tsx
+++ pages/console/examcentermappings/index.tsx
@@ -9,20 +9,34 @@ export default function ExamCenterMappings() {
   const [data, setData] = useState<MatrixDataType>([]);
   const [loader, setLoader] = useState<any>(false);
   const userData: any = useSelector((state: any) => state.data);
+  const [pagiData, setPagiData] = useState<any>({ page: 1, limit: 25 });
+
   let selectedCountry = userData?.selectedCountry?.label;
+
+  let payloadData = {
+    collection_name: "exam_center_mapping",
+    op_name: "find_many",
+    ...pagiData,
+    filter_var: {
+      country: selectedCountry,
+    },
+  };
 
   useEffect(() => {
     setLoader(true);
     async function readData() {
-      const examCenterMappings = await readExamCentersMapping();
+      const examCenterMappings = await readExamCentersMapping(undefined, undefined, payloadData);
       setData(examCenterMappings);
       setLoader(false);
     }
     readData();
-  }, [selectedCountry]);
+  }, [selectedCountry, pagiData.page, pagiData.limit]);
+
   return (
     <Container h={"100%"} fluid p={0}>
       <Matrix
+        setPagiData={setPagiData}
+        pagiData={pagiData}
         data={data.length > 0 ? data : []}
         setData={setData}
         showCreateForm={true}
