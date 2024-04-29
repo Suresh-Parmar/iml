50a0f8e0485857e33427fcd7f630cc2cc8efff63
djangir <djangir8875@gmail.com>
2024-04-29 21:56:57 +0530

    show rm pagination

--- pages/console/relationshipmanagers/index.tsx
+++ pages/console/relationshipmanagers/index.tsx
@@ -1,25 +1,52 @@
 import { Container } from "@mantine/core";
 import Matrix, { MatrixDataType } from "@/components/Matrix";
 import { useEffect, useState } from "react";
-import { readRelationshipManagers } from "@/utilities/API";
+import { readRelationshipManagers, readStudents } from "@/utilities/API";
 import Loader from "@/components/common/Loader";
 import { useSelector } from "react-redux";
+import { setGetData } from "@/helpers/getLocalStorage";
 
 export default function RelationshipManagers() {
   const [data, setData] = useState<MatrixDataType>([]);
   const [loader, setLoader] = useState<any>(false);
   const userData: any = useSelector((state: any) => state.data);
-  let selectedCountry = userData?.selectedCountry?.label;
+  let selectedCountry = userData?.selectedCountry?._id;
+
+  let savedPageSize = setGetData("pagesize");
+  if (savedPageSize) {
+    if (!isNaN(savedPageSize)) {
+      savedPageSize = Number(savedPageSize);
+    } else {
+      savedPageSize = 25;
+    }
+  }
+
+  const [totalrecords, setTotalrecords] = useState<any>({ totalPages: 1, pageSize: savedPageSize });
+  const [pagiData, setPagiData] = useState<any>({ page: 1, limit: savedPageSize || 25 });
+
+  let payload = {
+    collection_name: "users",
+    op_name: "find_many",
+    ...pagiData,
+    filter_var: {
+      role: "rm",
+      country_id: selectedCountry,
+    },
+  };
+
+  async function readData() {
+    setLoader(true);
+    const students = await readStudents(payload, true);
+    let limits = { total_count: students?.data?.total_count, total_pages: students?.data?.total_pages };
+    setTotalrecords(limits);
+    setData(students?.data?.response || []);
+    setLoader(false);
+  }
 
   useEffect(() => {
-    setLoader(true)
-    async function readData() {
-      const rm = await readRelationshipManagers();
-      setData(rm);
-    setLoader(false)
-    }
     readData();
-  }, [selectedCountry]);
+  }, [selectedCountry, Number(pagiData.page), Number(pagiData.limit)]);
+
   return (
     <Container h={"100%"} fluid p={0}>
       <Matrix
@@ -27,8 +54,12 @@ export default function RelationshipManagers() {
         setData={setData}
         showCreateForm={true}
         formType="Relationship Managers"
+        totalrecords={totalrecords}
+        setPagiData={setPagiData}
+        pagiData={pagiData}
+        showApiSearch={false}
       />
-      <Loader show={loader}/>
+      <Loader show={loader} />
     </Container>
   );
 }
